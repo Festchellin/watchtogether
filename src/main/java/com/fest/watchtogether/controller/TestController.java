8e8723275999ac9ff6409cec2eea01555af5a543
Festchellin <dreamerhaozhe@outlook.com>
2019-01-01 10:54:34 +0800

    add a TestController for testing the service is Available for user to access

--- src/main/java/com/fest/watchtogether/controller/TestController.java
+++ src/main/java/com/fest/watchtogether/controller/TestController.java
@@ -1,7 +1,15 @@
 package com.fest.watchtogether.controller;
 
+import com.fest.watchtogether.service.IUserService;
+import com.fest.watchtogether.util.AssembleUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName TestController
@@ -12,8 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+	@Autowired
+	private IUserService userService;
 	@GetMapping("/")
-	public Object index(){
+	public Object index() {
+		List<AssembleUtil.Condition> otherConditionList = new ArrayList<>();
+		AssembleUtil.Condition condition = new AssembleUtil.Condition();
+		condition.setConditionName("account");
+		condition.setConditionValue("asd");
+		otherConditionList.add(condition);
+		Map<String, Object> conditions = AssembleUtil.assembleConditions(0, 5, otherConditionList);
+		List userList = userService.getListByCondition(conditions);
+		userList.forEach(user -> System.out.println(user) );
 		return "index";
 	}
 }

29dfb4420806f232d5c91d8e40247d4c27d41f08
Festchellin <dreamerhaozhe@outlook.com>
2019-01-01 10:46:39 +0800

    add UserService Interface

---
+++ src/main/java/com/fest/watchtogether/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.fest.watchtogether.service;
+
+import com.fest.watchtogether.entity.User;
+
+/**
+ * @ClassName IUserService
+ * @Description
+ * @Author Festchellin
+ * @Date 12/31/2018  4:23 PM
+ * @Version 1.0
+ */
+public interface IUserService extends IBaseService<User> {
+}
